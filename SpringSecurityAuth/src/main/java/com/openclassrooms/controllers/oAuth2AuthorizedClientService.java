72e0bb376b9aa6663bccfca66e6e66ac6270bb96
julien382 <julhermain@gmail.com>
2024-02-06 22:54:35 +0100

    access token

---
+++ SpringSecurityAuth/src/main/java/com/openclassrooms/controllers/oAuth2AuthorizedClientService.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.controllers;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+
+public class oAuth2AuthorizedClientService {
+
+    public OAuth2AuthorizedClient loadAuthorizedClient(String authorizedClientRegistrationId, String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadAuthorizedClient'");
+    }
+
+}
